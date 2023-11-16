3f4fea7374020bc0f3e36466ede75f2f5ef7e34c
Tarun Atreya <53280371+Tarun-atreya@users.noreply.github.com>
2023-11-16 13:36:16 -0500

    Part 1

--- BookOrder.java
+++ BookOrder.java
@@ -1,3 +1,19 @@
 public class BookOrder {
-    
+    private String bookTitle;
+    private int quantity;
+
+    public BookOrder(String title, int quant)
+    {
+        bookTitle = title;
+        quantity = quant;
+    }
+
+    public String getBookTitle()
+    {
+        return bookTitle;
+    }
+    public int getQuantity()
+    {
+        return quantity;
+    }
 }
--- BookOrderProcessor.java
+++ BookOrderProcessor.java
@@ -1,3 +1,29 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BookOrderProcessor {
-    
+    private Queue<BookOrder> orderQueue;
+
+    // Constructor to initialize the orderQueue
+    public BookOrderProcessor() {
+        orderQueue = new LinkedList<BookOrder>();
+    }
+
+    // Your implementation for enqueueOrder method here
+    public void enqueueOrder(BookOrder order) {
+        orderQueue.add(order);
+    }
+
+    // Your implementation for processOrders method here
+    public void processOrders() {
+        while (!orderQueue.isEmpty()) {
+            BookOrder order = orderQueue.remove();
+            System.out.println("Title: " + order.getBookTitle() + " - " + order.getQuantity());
+        }
+    }
+
+    // Your implementation for getTotalOrders method here
+    public int getTotalOrders() {
+        return orderQueue.size();
+    }
 }
